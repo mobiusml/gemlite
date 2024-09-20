e93ef5599cd464067abfee9d73913824705d4cf2
mobicham <37179323+mobicham@users.noreply.github.com>
2024-09-20 11:52:30 +0200

    add vllm install in dep

--- examples/install_dependencies.sh
+++ examples/install_dependencies.sh
@@ -1,4 +1,5 @@
 #Install in this order
+pip install vllm;
 pip install bitblas;
 pip install torch==2.5.0.dev20240905+cu121 --index-url https://download.pytorch.org/whl/nightly/cu121; 
 pip install hqq;
