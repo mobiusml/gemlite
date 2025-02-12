194d68a5e00dd6c85c5b2a011fd0c574b8db8385
mobicham <hicham@mobiuslabs.com>
2025-02-12 16:17:27 +0000

    import helper

--- gemlite/__init__.py
+++ gemlite/__init__.py
@@ -3,6 +3,8 @@ __author__  = 'Dr. Hicham Badri'
 __credits__ = 'Mobius Labs GmbH'
 
 from .core import GemLiteLinearTriton, GemLiteLinear, DType, GEMLITE_ACC_DTYPE, GEMLITE_TRITON_CONFIG_CACHE, GEMLITE_TRITON_RESTRICT_M
+from .helper import A16W8, A8W8_int8_dynamic, A8W8_fp8_dynamic, A16Wn, A8Wn_dynamic
+
 load_config  = GemLiteLinear.load_config
 cache_config = GemLiteLinear.cache_config
 reset_config = GemLiteLinear.reset_config
