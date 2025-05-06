cc01cd24cbb8e99c1462479ab0f0e998c3bf053c
mobicham <hicham@mobiuslabs.com>
2025-05-06 07:54:05 +0000

    bump to 4.6.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='gemlite',
-    version="0.4.5",
+    version="0.4.6",
     url="https://github.com/mobiusml/gemlite/",
     author="Dr. Hicham Badri",
     author_email="hicham@mobiuslabs.com",
