a4eb27bacc01b19c55d78b01de8b8cc81c460b11
mobicham <hicham@mobiuslabs.com>
2025-06-18 10:59:23 +0000

    clean-up revsplit

--- gemlite/triton_kernels/gemv_revsplitK_A16fWnO16f_int32packing.py
+++ gemlite/triton_kernels/gemv_revsplitK_A16fWnO16f_int32packing.py
@@ -429,7 +429,7 @@ def gemv_revsplitK_A16fWnO16f_int32packing_forward(x: Tensor, W_q: Tensor, scale
     else:
         acc_dtype = DTYPE_TO_TRITON[acc_dtype]
 
-    compiled_kernel = gemv_revsplitK_A16fWnO16f_int32packing_kernel[grid](
+    gemv_revsplitK_A16fWnO16f_int32packing_kernel[grid](
         x, W_q, output,
         scales, zeros, scales_x,
         M, N, K, 
